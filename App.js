da19956392ee42ad28094492d894f40e1c1ca7bc
Imran Khan <imranjpj12@gmail.com>
2021-01-25 11:01:09 +0500

    Initital Commit

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ function App() {
   const [people, setPeople] = useState(data);
   return (
 
+
     <div className="d-flex justify-content-center mt-5">
         <div className="bg-light w-25 rounded pt-3">
           <h4 className="d-flex justify-content-center">{people.length} Birthdays today</h4>
